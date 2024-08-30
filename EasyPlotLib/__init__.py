781ba53480c02689ef31a26e6593c9008eeba2f0
HanYuyang <17766095120@163.com>
2024-08-30 14:32:35 +0800

    feat: add mplstyle file

--- EasyPlotLib/__init__.py
+++ EasyPlotLib/__init__.py
@@ -1,7 +1,24 @@
-from .figsizes_set import figsizes
-import scienceplots
-from .figsizes_set import subplot_labels
-from .cartopy_helper import cartopy_plot_tickmarks
-from .font_helper import set_custom_fonts
+import os
 
-set_custom_fonts()
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+
+from .cartopy_helper import cartopy_plot_tickmarks
+from .figsizes_set import figsizes, subplot_labels
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+font_dir = os.path.join(current_dir, "font")
+font_files = fm.findSystemFonts(fontpaths=[font_dir])
+for font_file in font_files:
+    fm.fontManager.addfont(font_file)
+
+# register all mplstyle
+style_dir = os.path.join(current_dir, "styles")
+stylesheets = {}
+for root, dirs, files in os.walk(style_dir):
+    new_stylesheets = plt.style.core.read_style_directory(root)
+    stylesheets.update(new_stylesheets)
+
+plt.style.core.update_nested_dict(plt.style.library, stylesheets)
+plt.style.core.available[:] = sorted(plt.style.library.keys())
