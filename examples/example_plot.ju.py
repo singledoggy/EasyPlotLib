7da9d527be7ed8b09dc1a0efd1112d9cfeb9eca7
HanYuyang <17766095120@163.com>
2024-08-30 21:34:08 +0800

    feat: update style

--- examples/example_plot.ju.py
+++ examples/example_plot.ju.py
@@ -7,7 +7,6 @@ from cnmaps import get_adm_maps
 nrows, ncols = 1, 2
 figsizes = epl.figsizes("nat2", nrows=nrows, ncols=ncols)
 
-# plt.style.use(["science", "no-latex"])
 plt.style.use(["nature"])
 plt.rcParams.update(figsizes)
 fig, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
@@ -15,55 +14,80 @@ fig, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
 for n, ax in enumerate(axs.flatten()):
     ax.plot([1.0, 2.0], [3.0, 4.0])
     ax.plot([1.0, 2.0], [2.0, 5.0])
-    ax.annotate(**epl.subplot_labels(n, "A"))
+    ax.annotate(**epl.subplot_labels(n, "a", fontsize=12))
 
-plt.savefig("test.png", dpi=300)
 # %%
-plt.rcParams["font.family"]
-# %%
-plt.rcParams["font.sans-serif"]
-# %%
-import matplotlib.pyplot as plt
-
+import cartopy.crs as ccrs
 import EasyPlotLib as epl
-
-nrows, ncols = 1, 1
-figsizes = epl.figsizes("nat2", nrows=nrows, ncols=ncols)
-
-plt.style.use(["science", "nature", "no-latex"])
-plt.rcParams.update(figsizes)
-plt.rcParams.update(
-    {
-        "font.family": "sans-serif",
-        # 'font.sans-serif': ['Helvetica'],
-    }
-)
-
-fig, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
-
-# for n, ax in enumerate(axs.flatten()):
-#       ax.annotate(**epl.subplot_labels(n, "a",fontsize=12))
-# %%
+import matplotlib.pyplot as plt
+import numpy as np
+from cnmaps import get_adm_maps
 
 nrows, ncols = 1, 2
-figsizes = epl.figsizes("nat2", inverted_aspect_ratio=1)
+figsizes = epl.figsizes("nat2")
 
-plt.style.use("default")
+plt.style.use("nature")
 plt.rcParams.update(figsizes)
 
 china_map = get_adm_maps(level="省", engine="geopandas")
+crs = ccrs.PlateCarree()
+
+
 fig, axs = plt.subplots(
     ncols=ncols,
     nrows=nrows,
     subplot_kw={"projection": ccrs.PlateCarree()},
 )
-for n, ax in enumerate(axs.flatten()):
-    china_map.plot(ax=ax, edgecolor="black", facecolor="none", linewidth=0.5)  # type: ignore
-    ax.annotate(**epl.subplot_labels(n, "A"))
+if nrows == 1 and ncols == 1:
+    axs = np.array([[axs]])
 
-width, height = fig.get_size_inches()
-inverted_aspect_ratio = height / width
+for n, ax in enumerate(axs.flatten()):
+    china_map.plot(ax=ax, edgecolor="black", facecolor="none", linewidth=0.5)
+    ax.annotate(**epl.subplot_labels(n, "a", fontsize=12))
+    gl = ax.gridlines(
+        draw_labels=True,
+        color="none",
+        linestyle="dotted",
+        transform=crs,
+    )
+    gl.top_labels = gl.right_labels = False
+    gl.rotate_labels = False
+
+    epl.cartopy_plot_tickmarks(ax, gl)
+
+# %%
+
+figsizes = epl.figsizes("nat2", nrows=2, ncols=2)
+
+plt.style.use("default")
+plt.rcParams.update(figsizes)
+
+china_map = get_adm_maps(level="省", engine="geopandas")
+crs = ccrs.PlateCarree()
+fig, axs = plt.subplot_mosaic(
+    """
+    bba
+    ccd
+    """,
+    per_subplot_kw={
+        ("b", "c"): {"projection": crs},
+    },
+)
+ax = axs["b"]
+ax.set_global()
+ax.coastlines()
+ax = axs["c"]
+ax.set_global()
+ax.coastlines()
+
+ax = axs["a"]
+ax.plot([1.0, 2.0], [3.0, 4.0])
+
+ax = axs["d"]
+ax.plot([1.0, 2.0], [3.0, 4.0])
+
+inverted_aspect_ratio = 0.7
 figsizes = epl.figsizes("nat2", inverted_aspect_ratio=inverted_aspect_ratio)
 fig.set_size_inches(figsizes["figure.figsize"])
-fig.savefig("test2.png", dpi=300)
+
 # %%
